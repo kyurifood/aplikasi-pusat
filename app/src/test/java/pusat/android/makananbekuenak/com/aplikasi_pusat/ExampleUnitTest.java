eae0204ca142f5765691cb72a3743834e27588e8
dadang <dadangiswan@gmail.com>
2016-03-10 11:44:40 +0700

    add new project aplikasi-pusat

---
+++ app/src/test/java/pusat/android/makananbekuenak/com/aplikasi_pusat/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package pusat.android.makananbekuenak.com.aplikasi_pusat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
