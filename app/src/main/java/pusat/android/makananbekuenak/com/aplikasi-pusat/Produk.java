0f117a57b20ed6ea80c52cf539d73eb953200586
gilang <gilang.mhs13@ti.poligon.ac.id>
2016-03-21 17:53:52 +0700

    Home Screen

---
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi-pusat/Produk.java
@@ -0,0 +1,14 @@
+package pusat.android.makananbekuenak.com.aplikasi_pusat;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Produk extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.produk);
+    }
+}
---
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi-pusat/Regional.java
@@ -0,0 +1,13 @@
+package pusat.android.makananbekuenak.com.aplikasi_pusat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Regional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.regional);
+    }
+}
